f6bfdbb642fae9ff184883bb3877802a47103de8
Gabriel Melgaco <gabrielmelgacom@gmail.com>
2024-09-30 20:47:00 -0300

    Implementação de banco de dados

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from peewee import *
+from datetime import datetime
+
+db = SqliteDatabase('dados.db')
+
 
 app = Flask(__name__)
+
+class Clientes(Model):
+    nome = CharField()
+    email = CharField()
+    telefone = CharField()
+    data = DateField()
+
+    class Meta:
+        database = db # This model uses the "people.db" database.
+
+db.connect()
+db.create_tables([Clientes])
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
+@app.route("/register", methods=['POST'])
+def registrar():
+    nome = request.form["nome"]
+    email = request.form["email"]
+    telefone = request.form["telefone"]
+    data = datetime.now()
+    ano = data.year
+    mes = data.month
+
+    Clientes.create(nome=nome, email=email, telefone=telefone, data=data)
+    return redirect(f'https://calendly.com/pedrorochabconsultoria/30min?month={ano}-{mes}')
+
 if __name__ == '__main__':
     app.run(debug=True)
+
