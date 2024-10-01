bc983d62750eff469639ee1ba0f82bf5672ee67e
Gabriel Melgaco <gabrielmelgacom@gmail.com>
2024-09-30 21:59:42 -0300

    Implementação de banco de dados

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template, request, redirect
 from peewee import *
 from datetime import datetime
+from flask_mail import Mail, Message
+
 
 db = SqliteDatabase('dados.db')
 
 
 app = Flask(__name__)
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False  # Deve ser False para o port 465
+app.config['MAIL_USE_SSL'] = True   # Deve ser True para o port 465
+app.config['MAIL_USERNAME'] = 'megdev99@gmail.com'
+app.config['MAIL_PASSWORD'] = '@13985261776Gm'
+app.config['MAIL_PASSWORD'] = 'lnxi sbiv dnrp hyym'
+
+
+mail = Mail(app)
 
 class Clientes(Model):
     nome = CharField()
@@ -32,7 +45,35 @@ def registrar():
     ano = data.year
     mes = data.month
 
+    # Registrar no banco de dados
     Clientes.create(nome=nome, email=email, telefone=telefone, data=data)
+    clientes = Clientes.select()
+    lista_clientes = []
+
+    for cliente in clientes:
+        cliente_info = f'Nome:{cliente.nome}, {cliente.email}, {cliente.telefone} às {cliente.data}'
+        lista_clientes.append(cliente_info)
+
+    # Criar mensagem de e-mail
+    mail_message = Message(
+        subject="Confirmação de Registro",
+        sender=app.config['MAIL_USERNAME'],  # Utilize a configuração do seu aplicativo
+        recipients=[email],  # Enviar e-mail para o usuário que preencheu o formulário
+        body=f"""
+            Confirmação de Registro
+            O usuário, {nome}, acabou de se registrar em seu site!
+            Recebemos seu telefone:{telefone} e e-mail: {email}.
+            
+            
+            Seu banco de dados de cliente é:
+            {lista_clientes}
+    """
+    )
+
+    # Enviar e-mail
+    mail.send(mail_message)
+
+    # Redirecionar após o registro
     return redirect(f'https://calendly.com/pedrorochabconsultoria/30min?month={ano}-{mes}')
 
 if __name__ == '__main__':
